93cb6b259fb003b2275f5c784966d3bf038a5a11
seanwlawrence <sean.w.lawrence@gmail.com>
2018-08-03 18:11:25 -0500

    Add tests and types

--- src/index.js
+++ src/index.js
@@ -1,148 +1,178 @@
 // @flow
 import type { Classnames, Entry } from './types';
 
-// holds all formatting methods
+// class with all formatting methods
 class BulmaClassname {
-	// key
-	selector: string;
-	//value
-	value: string | string[];
+	// object with keys and values from the API
+	classnames: Classnames;
 
 	// initialize contructor
-	constructor(selector: string, value: string | string[]) {
-		this.selector = selector;
-		this.value = value;
+	constructor(classnames: Classnames) {
+		this.classnames = classnames;
 	}
 
 	// formats single string values
-	formatString(selector: string, value: string): string | void {
+	formatString(selector: string, value: string = ''): string {
+		// check to see if the key (selector) matches the API, if so, return the formatted string, if not, return an empty string
 		switch (selector) {
-			case 'backgroundColor':
-				return `has-background-color-${value}`;
-			case 'textColor':
-				return `has-text-${value}`;
-			case 'textSize':
-				return `is-size-${value}`;
-			case 'textWeight':
-				return `has-text-weight-${value}`;
-			case 'textTransformation':
-				return `is-${value}`;
-			case 'textAlign':
-				return `has-text-${value}`;
-			case 'color':
-				return `is-color-${value}`;
-			case 'flex':
-				return `is-flex-${value}`;
-			case 'inlineFlex':
-				return `is-inline-flex-${value}`;
-			case 'block':
-				return `is-block-${value}`;
-			case 'inlineBlock':
-				return `is-inline-block-${value}`;
-			case 'inline':
-				return `is-inline-${value}`;
-			case 'columns':
-				return `columns ${value}`;
-			case 'column':
-				return `column is-${value}`;
-			case 'offset':
-				return `is-offset-${value}`;
-			case 'image':
-				return `image is-${value}`;
-			case 'is':
-				return `is-${value}`;
-			case 'has':
-				return `has-${value}`;
-			case 'raw':
-				return value;
-			default:
-				if (process.env.NODE_ENV === 'development') {
-					console.warn(
-						'An incorrect key was passed to bulmaClassnames. Refer to the API for a list of p' +
+		case 'backgroundColor':
+			return `has-background-${value}`;
+		case 'textColor':
+			return `has-text-${value}`;
+		case 'textSize':
+			return `is-size-${value}`;
+		case 'textWeight':
+			return `has-text-weight-${value}`;
+		case 'textTransformation':
+			return `is-${value}`;
+		case 'textAlign':
+			return `has-text-${value}`;
+		case 'color':
+			return `is-color-${value}`;
+		case 'flex':
+			return `is-flex-${value}`;
+		case 'inlineFlex':
+			return `is-inline-flex-${value}`;
+		case 'block':
+			return `is-block-${value}`;
+		case 'inlineBlock':
+			return `is-inline-block-${value}`;
+		case 'inline':
+			return `is-inline-${value}`;
+		case 'columns':
+			return `columns ${value}`;
+		case 'column':
+			return `column is-${value}`;
+		case 'offset':
+			return `is-offset-${value}`;
+		case 'image':
+			return `image is-${value}`;
+		case 'is':
+			return `is-${value}`;
+		case 'has':
+			return `has-${value}`;
+		case 'raw':
+			return value;
+		default:
+			if (process.env.NODE_ENV === 'development') {
+				// if in development mode, log a simple warning to the console
+				console.warn(
+					'An incorrect key was passed to bulmaclassnames. Refer to the API for a list of p' +
 							'ossible keys and values.',
-					);
-				}
+				);
+			}
+			// returns an empty string if key does not match the API
+			return '';
 		}
 	}
 
-	formatArray(selector: string, value: string[]): string | void {
+	// formats array values
+	formatArray(selector: string, value: string[]): string {
+		// adds a prefix to each item in the array
 		let addPrefixToAllValues = (prefix: string): string => {
-			return value.reduce(
-				(acc, next) => acc.concat(` ${prefix}-${next}`).trim(),
-				'',
-			);
+			return value.reduce((acc: string, next: string) => {
+				// reduces the array into a single string with the prefix added to the front of each work
+				return acc.concat(` ${prefix}-${next}`).trim();
+			}, '');
 		};
 
+		// check to see if the key (selector) matches the API, if so, return the formatted string, if not, return an empty string
 		switch (selector) {
-			case 'columns':
-				return `columns ${addPrefixToAllValues('is')}`;
-			case 'column':
-				return `column ${addPrefixToAllValues('is')}`;
-			case 'offset':
-				return addPrefixToAllValues('is-offset');
-			case 'flex':
-				return addPrefixToAllValues('is-flex');
-			case 'inlineFlex':
-				return addPrefixToAllValues('is-inline-flex');
-			case 'block':
-				return addPrefixToAllValues('is-block');
-			case 'inline-block':
-				return addPrefixToAllValues('is-inline-block');
-			case 'inline':
-				return addPrefixToAllValues('is-offset');
-			case 'image':
-				return `image ${addPrefixToAllValues('is')}`;
-			case 'is':
-				return addPrefixToAllValues('is');
-			case 'has':
-				return addPrefixToAllValues('has');
-			case 'textSize':
-				return addPrefixToAllValues('is-size');
-			default:
-				if (process.env.NODE_ENV === 'development') {
-					console.warn(
-						'An incorrect key was passed to bulmaClassnames. Refer to the API for a list of p' +
+		case 'column':
+			return `column ${addPrefixToAllValues('is')}`;
+		case 'offset':
+			return addPrefixToAllValues('is-offset');
+		case 'flex':
+			return addPrefixToAllValues('is-flex');
+		case 'inlineFlex':
+			return addPrefixToAllValues('is-inline-flex');
+		case 'block':
+			return addPrefixToAllValues('is-block');
+		case 'inlineBlock':
+			return addPrefixToAllValues('is-inline-block');
+		case 'inline':
+			return addPrefixToAllValues('is-inline');
+		case 'is':
+			return addPrefixToAllValues('is');
+		case 'has':
+			return addPrefixToAllValues('has');
+		default:
+			if (process.env.NODE_ENV === 'development') {
+				// if in development mode, log a simple warning to the console
+				console.warn(
+					'An incorrect key was passed to bulmaclassnames. Refer to the API for a list of p' +
 							'ossible keys and values.',
-					);
-				}
+				);
+			}
+			// returns an empty string if key does not match the API
+			return '';
 		}
 	}
 
-	formatClassname(selector: string, value: string | string[]): string | void {
+	// controls whether the formatString or formatARray method should be called based on the value
+	formatClassname(selector: string, value: string | string[] = ''): string {
+		// if value is a string, pass it into the formatString method
 		if (typeof value === 'string') {
 			return this.formatString(selector, value);
-		} else if (
+		}
+		// if value is an array of only strings, pass it into the formatArray method
+		else if (
 			Array.isArray(value) &&
 			value.map((entry) => typeof entry === 'string')
 		) {
 			return this.formatArray(selector, value);
-		} else if (process.env.NODE_ENV === 'development') {
+		}
+		// if value if neither of the above, log a warning to the console and return an empty string
+		else if (process.env.NODE_ENV === 'development') {
+			// if in development mode, log a simple warning to the console
 			console.warn(
-				'Invalid value has been passed to bulmaClassnames. Values can only be a string or' +
+				'Invalid value has been passed to bulmaclassnames. Values can only be a string or' +
 					' and array of strings',
 			);
 		}
+		// value is not a string or an array of only strings, return an empty string
+		return '';
 	}
 
-	get value(): string | void {
-		return this.formatClassname(this.selector, this.value);
+	// applies the formatClassname method to all of the entries passed in to the classnames object
+	main(): string {
+		// imperative version of Object.entries to pass type checks with Flow
+		let result = Object.keys(this.classnames)
+			.map((key) => {
+				return [key, this.classnames[key]];
+			})
+			// run each key:value pair into the formatClassname ethod
+			.map((entry) => {
+				// destructure the key (selector) and value (value) to pass into formatClassname
+				const [
+					selector: string,
+					value: [string, string] | [string, string[]],
+				] = entry;
+
+				// return formattedClassName
+				return this.formatClassname(selector, value);
+			});
+
+		// if there is at least one key:value pair passed, then reduce all values into a single string
+		if (Object.keys(this.classnames).length !== 0) {
+			result = result.reduce((acc, next) => {
+				return acc.concat(` ${next}`).trim();
+			});
+		}
+
+		// check that result is a non-empty string, and return it if so or an empty string if not
+		return typeof result === 'string' && result.length > 1 ? result : '';
+	}
+
+	// run the main method as a getter
+	get result(): string {
+		return this.main();
 	}
 }
 
-const bc = (classNames: Classnames): string | void => {
-	if (Object.entries(classNames).length > 0) {
-		return Object.entries(classNames)
-			.map(
-				(entry: Entry): string => {
-					const [selector, value] = entry;
-					const className = new BulmaClassname(selector, value);
-					return className.value;
-				},
-			)
-			.reduce((acc: string, next: string) => {
-				return acc.concat(` ${next}`);
-			});
-	}
-};
+// pass in the object of classnames and export the formatted string
+const bc = (classnames: Classnames): string =>
+	new BulmaClassname(classnames).result;
 
+// default export so it can be imported like `import bc from 'bulma-classnames'`
 export default bc;
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,5 +1,87 @@
 import bc from './index.js';
 
-test('formats an array value', () => {});
+// object with only single string values
+const testStringValues = {
+	is: 'clearfix',
+	has: 'text-white',
+	backgroundColor: 'primary',
+	textColor: 'primary',
+	textSize: '7',
+	textWeight: 'bold',
+	textTransformation: 'uppercase',
+	textAlign: 'left',
+	color: 'primary',
+	column: '11-mobile',
+	offset: '1-mobile',
+	flex: 'mobile',
+	inlineFlex: 'mobile',
+	block: 'mobile',
+	inlineBlock: 'mobile',
+	inline: 'mobile',
+	image: 'half',
+	raw: 'is-primary',
+};
 
-test('formats a string value', () => {});
+// object with only array values
+const testArrayValues = {
+	is: ['clearfix', 'marginless'],
+	has: ['text-white', 'background-white'],
+	column: ['11-mobile', '10-tablet', '9-desktop'],
+	offset: ['11-mobile', '10-tablet', '9-desktop'],
+	flex: ['mobile', 'tablet', 'desktop'],
+	inlineFlex: ['mobile', 'tablet', 'desktop'],
+	block: ['mobile', 'tablet', 'desktop'],
+	inlineBlock: ['mobile', 'tablet', 'desktop'],
+	inline: ['mobile', 'tablet', 'desktop'],
+};
+
+// object with array values, where only a string should be passed
+const testStringOnlyValues = {
+	backgroundColor: ['primary', 'link'],
+	textColor: ['primary', 'link'],
+	textSize: ['7', '5'],
+	textWeight: ['bold', 'semibold'],
+	textTransformation: ['uppercase', 'lowercase'],
+	textAlign: ['left', 'right'],
+	color: ['primary', 'link'],
+	image: ['half', 'two-thirds'],
+	raw: ['is-primary', 'is-link'],
+};
+
+test('formats a string value', () => {
+	const result = bc(testStringValues);
+
+	expect(result).toBe(
+		'is-clearfix has-text-white has-background-primary has-text-primary is-size-7 has-text-weight-bold is-uppercase has-text-left is-color-primary column is-11-mobile is-offset-1-mobile is-flex-mobile is-inline-flex-mobile is-block-mobile is-inline-block-mobile is-inline-mobile image is-half is-primary',
+	);
+});
+
+test('formats an array value', () => {
+	const result = bc(testArrayValues);
+
+	expect(result).toBe(
+		'is-clearfix is-marginless has-text-white has-background-white column is-11-mobile is-10-tablet is-9-desktop is-offset-11-mobile is-offset-10-tablet is-offset-9-desktop is-flex-mobile is-flex-tablet is-flex-desktop is-inline-flex-mobile is-inline-flex-tablet is-inline-flex-desktop is-block-mobile is-block-tablet is-block-desktop is-inline-block-mobile is-inline-block-tablet is-inline-block-desktop is-inline-mobile is-inline-tablet is-inline-desktop',
+	);
+});
+
+test('returns empty string on keys that don\'t accept array values', () => {
+	const result = bc(testStringOnlyValues);
+	expect(result).toBe('');
+});
+
+test('returns empty string on empty objects', () => {
+	const result = bc({});
+	expect(result).toBe('');
+});
+
+test('formats string properly with a single key:value pair', () => {
+	const result = bc({ is: 'color-primary' });
+
+	expect(result).toBe('is-color-primary');
+});
+
+test('formats string properly with a more than one key:value pair', () => {
+	const result = bc({ is: 'color-primary', has: 'text-white' });
+
+	expect(result).toBe('is-color-primary has-text-white');
+});
